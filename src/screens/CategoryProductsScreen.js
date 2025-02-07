6a693557f28647d5e506927634db63e68f0ad158
Abdella Boutaarourt <abdella.boutaarourt@student.ehb.be>
2025-02-07 22:02:48 +0100

    fix product screen background

--- src/screens/CategoryProductsScreen.js
+++ src/screens/CategoryProductsScreen.js
@@ -60,13 +60,38 @@ const CategoryProductsScreen = ({ route }) => {
 
     return (
         <View style={styles.container}>
-            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} contentContainerStyle={styles.filterContainer} style={styles.scrollView}>
+            <View style={styles.header}>
+                <TouchableOpacity style={styles.sortButton}>
+                    <svg width="13" height="13" viewBox="0 0 11 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M5.85349 3.1465C6.04899 2.951 6.04899 2.635 5.85349 2.4395L3.70704 0.293C3.51204 0.0980005 3.25605 0.000499725 3.00006 0.000499725C2.74406 0.000499725 2.48807 0.0980005 2.29307 0.293L0.146622 2.4395C-0.0488739 2.635 -0.0488739 2.951 0.146622 3.1465C0.342117 3.342 0.65811 3.342 0.853606 3.1465L2.50007 1.5V11.5C2.50007 11.7765 2.72406 12 3.00006 12C3.27605 12 3.50005 11.7765 3.50005 11.5V1.5L5.14651 3.1465C5.342 3.342 5.658 3.342 5.85349 3.1465ZM10.8534 9.5605L8.70693 11.707C8.31744 12.0965 7.68245 12.0965 7.29296 11.707L5.14651 9.5605C4.95101 9.365 4.95101 9.049 5.14651 8.8535C5.342 8.658 5.658 8.658 5.85349 8.8535L7.49995 10.5V0.5C7.49995 0.2235 7.72345 0 7.99994 0C8.27644 0 8.49993 0.2235 8.49993 0.5V10.5L10.1464 8.8535C10.2439 8.756 10.3719 8.707 10.4999 8.707C10.6279 8.707 10.7559 8.756 10.8534 8.8535C11.0489 9.049 11.0489 9.365 10.8534 9.5605Z" fill="black" />
+                    </svg>
+
+                    <Text style={styles.sortText}>Trier</Text>
+                </TouchableOpacity>
+                <View style={styles.separator} />
+
+                <Text style={styles.resultCount}>{filteredProducts.length} Résultats</Text>
+
+                <View style={styles.separator} />
+
+                <TouchableOpacity style={styles.sortButton}>
+                    <MaterialIcons name="filter-list" size={20} color="black" />
+                    <Text style={styles.sortText}>Filtre</Text>
+                </TouchableOpacity>
+            </View>
+
+            <ScrollView
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                style={styles.filterContainer}
+                contentContainerStyle={{ paddingBottom: 5 }}
+            >
                 {filters.map((filter, index) => (
                     <TouchableOpacity
                         key={index}
                         onPress={() => setSelectedFilter(filter)}
                         style={[
-                            styles.filterButtonn,
+                            styles.filterButton,
                             selectedFilter === filter && styles.activeFilter,
                         ]}
                     >
@@ -81,28 +106,9 @@ const CategoryProductsScreen = ({ route }) => {
                     </TouchableOpacity>
                 ))}
             </ScrollView>
-            <View style={styles.header}>
-                <TouchableOpacity style={styles.sortButton}>
-                    <svg width="16" height="16" viewBox="0 0 11 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M5.85349 3.1465C6.04899 2.951 6.04899 2.635 5.85349 2.4395L3.70704 0.293C3.51204 0.0980005 3.25605 0.000499725 3.00006 0.000499725C2.74406 0.000499725 2.48807 0.0980005 2.29307 0.293L0.146622 2.4395C-0.0488739 2.635 -0.0488739 2.951 0.146622 3.1465C0.342117 3.342 0.65811 3.342 0.853606 3.1465L2.50007 1.5V11.5C2.50007 11.7765 2.72406 12 3.00006 12C3.27605 12 3.50005 11.7765 3.50005 11.5V1.5L5.14651 3.1465C5.342 3.342 5.658 3.342 5.85349 3.1465ZM10.8534 9.5605L8.70693 11.707C8.31744 12.0965 7.68245 12.0965 7.29296 11.707L5.14651 9.5605C4.95101 9.365 4.95101 9.049 5.14651 8.8535C5.342 8.658 5.658 8.658 5.85349 8.8535L7.49995 10.5V0.5C7.49995 0.2235 7.72345 0 7.99994 0C8.27644 0 8.49993 0.2235 8.49993 0.5V10.5L10.1464 8.8535C10.2439 8.756 10.3719 8.707 10.4999 8.707C10.6279 8.707 10.7559 8.756 10.8534 8.8535C11.0489 9.049 11.0489 9.365 10.8534 9.5605Z" fill="black" />
-                    </svg>
-
-                    <Text style={styles.sortText}>Trier</Text>
-                </TouchableOpacity>
-                <View style={styles.separator} />
-
-                <Text style={styles.resultCount}>{filteredProducts.length} Résultats</Text>
-
-                <View style={styles.separator} />
-
-                <TouchableOpacity style={styles.filterButton}>
-                    <MaterialIcons name="filter-list" size={20} color="black" />
-                    <Text style={styles.filterText}>Filtre</Text>
-                </TouchableOpacity>
-            </View>
 
             {filteredProducts.length === 0 ? (
-                <View style={{ alignItems: 'center', marginTop: 20 }}>
+                <View style={{ alignItems: 'center', flex: 1 }}>
                     <Text style={{ fontSize: 16, color: 'gray' }}>Aucun produit trouvé</Text>
                 </View>
             ) : (
@@ -111,27 +117,27 @@ const CategoryProductsScreen = ({ route }) => {
                     data={filteredProducts}
                     keyExtractor={(item) => item.id.toString()}
                     numColumns={2}
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{ backgroundColor: '#F1F5F9', paddingVertical: 10, paddingHorizontal: 10, flex: 1 }}
                     renderItem={({ item }) => (
                         <View style={styles.item}>
                             <View style={styles.itemContainer}>
+
+                                <TouchableOpacity
+                                    onPress={() => handleAddToFavorites(item.id)}
+                                    style={styles.iconButton}
+                                >
+                                    <Ionicons name="heart-outline" size={24} color="#08744E" />
+                                </TouchableOpacity>
                                 <View style={styles.imageContainer}>
                                     <Image source={{ uri: item.image }} style={styles.image} />
                                 </View>
-                                <View style={styles.iconContainer}>
-                                    <TouchableOpacity
-                                        onPress={() => handleAddToFavorites(item.id)}
-                                        style={styles.iconButton}
-                                    >
-                                        <Ionicons name="heart-outline" size={24} color="#08744E" />
-                                    </TouchableOpacity>
-
-                                    <TouchableOpacity
-                                        onPress={() => handleAddToCart(item.id)}
-                                        style={styles.CartButton}
-                                    >
-                                        <FontAwesome name="cart-plus" size={24} color="white" />
-                                    </TouchableOpacity>
-                                </View>
+                                <TouchableOpacity
+                                    onPress={() => handleAddToCart(item.id)}
+                                    style={styles.CartButton}
+                                >
+                                    <FontAwesome name="cart-plus" size={24} color="white" />
+                                </TouchableOpacity>
                             </View>
 
                             <Text style={styles.itemName}>{item.name}</Text>
@@ -148,32 +154,38 @@ const CategoryProductsScreen = ({ route }) => {
 };
 
 const styles = StyleSheet.create({
-    scrollView: {
-        maxHeight: 45,
-    },
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#ffff',
+        flex: 1
     },
     filterContainer: {
         flexDirection: "row",
-        alignItems: 'center',
-        alignContent: 'center',
-        borderBottomWidth: 1,
-        borderBottomColor: "#CFCFCF",
-        paddingHorizontal: 16
-
+        marginBottom: 10,
+        height: 45,
+        marginHorizontal: 16,
+        flexGrow: 0
     },
-    filterButtonn: {
+    filterButton: {
         paddingHorizontal: 15,
-        paddingVertical: 4,
-        borderRadius: 20,
+        borderWidth: 1,
+        height: 30,
+        borderColor: "#CCCCCC",
+        borderRadius: 7,
+        marginRight: 10,
+        justifyContent: "center",
     },
     activeFilter: {
-        backgroundColor: "#DEEDE0",
+        backgroundColor: "#01A96E",
+        borderWidth: 0,
+    },
+    filterText: {
+        fontSize: 14,
+        fontFamily: "InterRegular",
+        textAlign: "center",
+        color: "black",
     },
     activeFilterText: {
-        color: "#008E5C",
+        color: "#fff",
     },
     loadingContainer: {
         flex: 1,
@@ -185,8 +197,9 @@ const styles = StyleSheet.create({
         justifyContent: 'space-around',
         alignItems: 'center',
         marginBottom: 16,
-        padding: 16,
-
+        padding: 8,
+        borderBottomWidth: 1,
+        borderColor: "#CFCFCF",
     },
     separator: {
         height: 30,
@@ -197,32 +210,23 @@ const styles = StyleSheet.create({
     sortButton: {
         flexDirection: 'row',
         alignItems: 'center',
+        gap: 10,
     },
     sortText: {
-        marginLeft: 4,
-        fontSize: 15,
+        fontSize: 11,
         fontFamily: "InterRegular"
     },
     resultCount: {
-        fontSize: 15,
+        fontSize: 11,
         fontFamily: "InterRegular"
     },
-    filterButton: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    filterText: {
-        marginLeft: 4,
-        fontSize: 15,
-        fontFamily: "InterRegular"
-    },
+
     item: {
         flex: 1,
         margin: 4,
         alignItems: 'center',
         backgroundColor: 'white',
         borderRadius: 10,
-        padding: 10,
         borderWidth: 1,
         borderColor: '#e0e0e0',
         position: 'relative',
@@ -233,19 +237,28 @@ const styles = StyleSheet.create({
         borderRadius: 10,
     },
     imageContainer: {
-        alignItems: 'center'
+        alignItems: 'center',
+        width: "100%"
     },
     itemContainer: {
         flex: 1,
-        width: '100%'
+        width: '100%',
+        flexDirection: 'column',
+        justifyContent: "space-between",
+        alignItems: 'flex-end',
+        borderBottomWidth: 1,
+        borderColor: "#C1C2C1",
     },
     itemName: {
-        marginVertical: 4,
+        paddingHorizontal: 10,
+        marginTop: 10,
+        marginBottom: 5,
         fontSize: 14,
         fontFamily: 'InterSemiBold',
         textAlign: 'left',
     },
     itemPrice: {
+        paddingHorizontal: 10,
         marginTop: 5,
         fontSize: 14,
         fontFamily: 'InterSemiBold',
@@ -256,6 +269,8 @@ const styles = StyleSheet.create({
 
     },
     itemMinQuantity: {
+        paddingHorizontal: 10,
+        marginBottom: 7,
         width: '100%',
         fontSize: 11,
         color: '#666',
@@ -263,16 +278,15 @@ const styles = StyleSheet.create({
         textAlign: 'left',
 
     },
-    iconContainer: {
-        flexDirection: 'column',
-        alignItems: "flex-end",
-    },
     iconButton: {
         padding: 8,
+        backgroundColor: '#F2F2F2',
+        width: "fit-content",
+        borderTopRightRadius: 10
     },
     CartButton: {
         padding: 8,
-        backgroundColor: '#08744E'
+        backgroundColor: '#01A96E'
     },
 });
 
